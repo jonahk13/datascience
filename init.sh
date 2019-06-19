128c2bd0c5922fff4d545675601dedf1066f0361
jonahk13 <34174474+jonahk13@users.noreply.github.com>
2019-06-19 12:38:30 -0400

    update

--- init.sh
+++ init.sh
@@ -2,4 +2,3 @@
 sudo apt-get update
 git config --global user.email "jonahk13@hotmail.com"
 git config --global user.name "Jonah Keslassy"
-datalab create mydsinstance --zone northamerica-northeast1-a
