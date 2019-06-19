9f5c2da31c0acdff0ef993f9b2bc6435977adbfe
jonahk13 <34174474+jonahk13@users.noreply.github.com>
2019-06-19 12:04:15 -0400

    commit 2

--- init.sh
+++ init.sh
@@ -2,6 +2,4 @@
 sudo apt-get update
 git config --global user.email "jonahk13@hotmail.com"
 git config --global user.name "Jonah Keslassy"
-gcloud components update
-gcloud components install datalab
 datalab create mydsinstance --zone northamerica-northeast1-a
